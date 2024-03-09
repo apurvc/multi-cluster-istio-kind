93917d6e69fad63834d1b833d6159bab86e98628
Apurv Chandra <apurvc@gmail.com>
2024-03-09 23:13:51 +0000

    fixes with latest kind and istio

--- kind-setup/install-metallb.sh
+++ kind-setup/install-metallb.sh
@@ -10,9 +10,9 @@ NUM_CLUSTERS="${NUM_CLUSTERS:-2}"
 
 for i in $(seq "${NUM_CLUSTERS}"); do
   echo "Starting metallb deployment in cluster${i}"
-  kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/master/manifests/namespace.yaml --context "cluster${i}"
-  kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"   --context "cluster${i}"
-  kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/master/manifests/metallb.yaml --context "cluster${i}"
-  kubectl apply -f ./metallb-configmap-${i}.yaml --context "cluster${i}"
+  kubectl create ns metallb-system --context "kind-cluster${i}"
+  kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"   --context "kind-cluster${i}"
+  kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.14.3/config/manifests/metallb-native.yaml --context "kind-cluster${i}"
+  kubectl apply -f ./metallb-configmap-${i}.yaml --context "kind-cluster${i}"
   echo "----"
 done
